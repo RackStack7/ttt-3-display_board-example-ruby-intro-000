cedbbb2755dcfe596686b252fab05ad357adf0d2
James Daaran <jamesdaaran@gmail.com>
2021-03-31 11:38:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,19 +1,9 @@
 puts "Welcome to Tic Tac Toe"
-puts "   |   |   "
- puts "-----------"
-puts "   |   |   "
- puts "-----------"
-puts "   |   |   "
 def display_board
-  display_board = []
-  row1 =[]
-  row1 = [   ,   ,   ]
-   puts "-----------"
-  row2 = []
-  row2 = [   ,   ,   ]
-   puts "-----------"
-  row3 = []
-  row3 =  [   ,   ,   ]
-  display_board = [row1, row2, row3]
-  return display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
+display_board
